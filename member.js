589160f8402dff58d045772d5f3fd196187a6578
AnssiIlari <127083657+AnssiIlari@users.noreply.github.com>
2023-12-23 11:50:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  return [
+    {
+      id: 1,
+      name: 'JavaScript',
+      level: 3
+    },
+    {
+      id: 2,
+      name: 'React',
+      level: 2
+    },
+    {
+      id: 3,
+      name: 'NodeJS',
+      level: 1
+    }
+  ];
+}
